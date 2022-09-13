8c08730d9e51ba511dfeac86082c7873ecef7021
Alexandr Dmitrenko <a.dmitrenko@trinitygroup.ru>
2022-09-13 11:59:52 +0300

    notification

--- js/task4_4.js
+++ js/task4_4.js
@@ -11,45 +11,34 @@ function useRequest(width, heigth) {
     return fetch(` https://loremflickr.com/json/g/${width}/${heigth}/all`)
         .then(response => response.json())
         .then(json => console.log(json))
-        .catch(() => { console.log('error') });
+        .catch(() => {
+            console.log('error')
+        });
 }
 
 //вещаем событие клика на кнопку
 btn.addEventListener('click', () => {
     let imgWidth = parseInt((firstVal.value), 10);
     let imgHeight = parseInt((secondVal.value), 10);
-
-    useRequest(320, 240);
-
     showNotification(imgWidth, imgHeight);
-
-    // let fieldValue = field.value;
-    // let number;
-    // if (fieldValue != '') {
-    //     number = parseInt(fieldValue, 10);
-    //     //если в поле введена не цыфра
-    //     showNotification(number);
-    // } else {
-    //     //если поле пустое
-    //     showNotification();
-    // }
 })
 
 //вызываем окно об ошибке
 function showNotification(width, heigts) {
 
-    if(!isNaN(width) || !isNaN(heigts)) {
-        //если введены цыфры вне диапазона
-        if (width < 100 || width > 500 && heigts < 100 || heigts > 500) {
+    if (!isNaN(width) || !isNaN(heigts)) {
+
+        if ((width >= 100 && width <= 500) && (heigts >= 100 && heigts <= 500)) {
+            useRequest(width, heigts);
+        } else {
+            //если введены цыфры вне диапазона
             massage.classList.add('main__msg--active');
             massage.innerText = 'Значение вне диапазона!';
-        } else {
-            massage.classList.remove('main__msg--active');
-            massage.innerText = ' ';
         }
-        //если введена не цыфра
+
     } else {
+        //если введена не цыфра
         massage.classList.add('main__msg--active');
-        massage.innerText = 'Вы ввели неверное значение первого поля';
+        massage.innerText = 'Вы ввели неверное полей';
     }
 }
